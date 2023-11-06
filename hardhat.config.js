374c4381b98ec2becdfa575f956cfdb3fddc9a5d
0xchandanb <130664342+0xchandanb@users.noreply.github.com>
2023-11-06 08:35:55 +0000

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,29 @@ require('@nomicfoundation/hardhat-ignition')
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.17',
+
+  defaultNetwork: "buildbear",
+
+  networks: {
+    hardhat: {},
+    buildbear: {
+      url: "https://rpc.dev.buildbear.io/spiritual-wicket-systri-warrick-f9c2771d",
+    },
+  },
+
+  etherscan: {
+    apiKey: {
+      buildbear: "verifyContract",
+    },
+    customChains: [
+      {
+        network: "buildbear",
+        chainId: 1,
+        urls: {
+          apiURL: "https://rpc.buildbear.io/verify/etherscan/spiritual-wicket-systri-warrick-f9c2771d",
+          browserURL: "https://explorer.buildbear.io/spiritual-wicket-systri-warrick-f9c2771d",
+        },
+      },
+    ],
+  },
 }
